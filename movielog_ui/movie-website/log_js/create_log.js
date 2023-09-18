f343a501a7194ea50cc664540545a22bf11d788f
hemaraavi <hemaraavi060606@gmail.com>
2023-09-18 14:05:57 +0530

    changes

--- movielog_ui/movie-website/log_js/create_log.js
+++ movielog_ui/movie-website/log_js/create_log.js
@@ -1,1 +1,11 @@
-var editor = new FroalaEditor('#froala');
+tinymce.init({
+    selector: '#mytextarea',
+
+    a_plugin_option: true,
+    a_configuration_option: 400,
+    plugins: 'a_tinymce_plugin a11ychecker image advcode   casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinycomments ',
+    toolbar: 'addcomment showcomments  code casechange checklist  export  image editimage pageembed  table tableofcontents',
+    toolbar_mode: 'floating',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+  });
